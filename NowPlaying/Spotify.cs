b654581de89c569afdc0390c93ec963bc9deeabf
nenohi <kimutipartylove@gmail.com>
2022-01-18 11:20:36 +0900

    Spotify Token Refresh

--- NowPlaying/Spotify.cs
+++ NowPlaying/Spotify.cs
@@ -96,7 +96,15 @@ namespace NowPlaying
         public async Task<CurrentlyPlaying> GetCurrentlyPlaying()
         {
             if (spotifyClient == null) return Playing;
-            Playing = await spotifyClient.Player.GetCurrentlyPlaying(new PlayerCurrentlyPlayingRequest(PlayerCurrentlyPlayingRequest.AdditionalTypes.Track));
+            try
+            {
+                Playing = await spotifyClient.Player.GetCurrentlyPlaying(new PlayerCurrentlyPlayingRequest(PlayerCurrentlyPlayingRequest.AdditionalTypes.Track));
+            }
+            catch (APIUnauthorizedException e)
+            {
+                await RefreshToken();
+                Playing = await spotifyClient.Player.GetCurrentlyPlaying(new PlayerCurrentlyPlayingRequest(PlayerCurrentlyPlayingRequest.AdditionalTypes.Track));
+            }
             return Playing;
         }
 
